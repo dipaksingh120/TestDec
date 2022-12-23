44204c0b60e59ac0ed0ce543a38ec2f84b73f6e9
Dipak <dipusingh2020@gmail.com>
2022-12-23 10:10:52 +0530

    begin comit 2

---
+++ GitMain/src/step1/begin.java
@@ -0,0 +1,21 @@
+package step1;
+
+public class begin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello java");
+		System.out.println("hello java world2");
+byte a=10;
+ byte b=10;
+ byte c=(byte)(a+b);
+System.out.println(c);
+System.out.println(a);
+String s="arun kumar";
+String a1[] = s.split(" ");
+System.out.println(a1[0]);
+System.out.println(s.substring(0,4));
+System.out.println(s.substring(5,10));
+	}
+
+}
